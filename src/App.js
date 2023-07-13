4f538dcbc7f173c3e859dfb4fc01fce3bef63c15
Your Name <you@example.com>
2023-07-13 19:45:54 +0900

    pictures working

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useEffect, useState } from "react"
-import axios from "axios";
+import Panel from "./Panel.js"
 
 function App() {
   return (
     <div className="App">
       <Panel oldnew="old"></Panel>
       <Or></Or>
-      <GetApiData></GetApiData>
       <Panel oldnew="new"></Panel>
 
     </div>
   );
 }
 
-function Panel(props){
-  let hell = props.oldnew;
-
-  const clickPanel  = function(){
-    console.log(hell)
-
-  }
-
-  return(
-    <div className="panel" onClick={clickPanel}>
-      
-      <h1>{props.oldnew}<GetApiData></GetApiData></h1>
-    </div>
-  )
-}
 
 function Or(){
   return (
@@ -37,43 +19,6 @@ function Or(){
       <h2 className="Or">OR</h2>
     </div>
   )
-}
-
-function GetApiData(){
-  const [data,setData] = useState();
-
-  useEffect(() => {
-
-    async function hello (){
-      const url = 'https://moviesdatabase.p.rapidapi.com/titles/tt12343534/ratings';
-      const options = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': '3d73a62809msh2aaaeab14d79a03p1edeabjsn3ff2daa48d59',
-          'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-        }
-        
-      };
-      try{
-        const resp = await fetch(url,options)
-        const data = await resp.json()
-        setData(data.results)
-        return data
-      }
-      catch(err){
-        console.log(err)
-      }
-    }
-      const a = hello();
-  },[]);
-
-  console.log(data.averageRating)
-
-  return (
-    <div>
-      <h1>{data.averageRating}</h1>
-    </div>
-  );
 }
 
 export default App;
