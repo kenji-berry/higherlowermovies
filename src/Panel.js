2596ab1a2cccbc5a85d1567d71c5464261ac7fb2
Your Name <you@example.com>
2023-07-22 00:36:32 +0900

    rating goes up animated, not put in place properly

--- src/Panel.js
+++ src/Panel.js
@@ -3,6 +3,22 @@ import React, { useEffect, useState } from "react"
 
 
 function Panel(props){
+  console.log(props)
+  const showRating= () =>{
+    let startValue=0;
+    let endValue=props.movie.rating;
+    let duration = 30
+    let counter = setInterval(function(){
+      duration+=100
+      startValue+=0.1;
+      startValue = Math.round(startValue*10)/10
+      document.getElementById("rating").textContent=startValue;
+      if(startValue === endValue){
+        clearInterval(counter)
+      }
+    }, duration)
+  }
+
     console.log(props)
     let movie = props.movie
     let clickfunction = props.onClick
@@ -10,13 +26,18 @@ function Panel(props){
     console.log(movie)
 
     return(
+      <div>
         <div className='panel' id='panel' onClick={event => clickfunction[0](props.panelNumber,props.panelNumArr,clickfunction[1],props.setPanelNum)} style={{backgroundImage: `url(${movie.image})`, backgroundSize: "cover", backgroundPosition: "center center"}}>  
             <div id="panel-text">
               <div>{movie.name} ({movie.year})</div>
-              <div>({movie.rating})</div>
+              <div id="rating">;</div>
               <div>{movie.numVotes}</div>
+
             </div>  
         </div>
+        <button onClick={showRating}>hello</button>
+      </div>
+
     )
   }
 
