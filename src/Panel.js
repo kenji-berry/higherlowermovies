9cd2046d5ec6b82917c2c8f49fab87f84dd3ede9
Your Name <you@example.com>
2023-07-22 03:38:14 +0900

    working up works both panels

--- src/Panel.js
+++ src/Panel.js
@@ -4,20 +4,28 @@ import React, { useEffect, useState } from "react"
 
 function Panel(props){
   console.log(props)
-  const showRating= () =>{
-    document.getElementById("rating").style.opacity="100"
-    let startValue=1;
-    let endValue=props.movie.rating;
-    let duration = 30
-    let counter = setInterval(function(){
-      duration+=100
-      startValue+=0.1;
-      startValue = Math.round(startValue*10)/10
-      document.getElementById("rating").textContent=startValue;
-      if(startValue === endValue){
-        clearInterval(counter)
-      }
-    }, duration)
+  const test = () =>{
+    let ratingsShow = document.getElementsByClassName("rating")
+    ratingsShow  = Array.prototype.slice.call(ratingsShow)
+    
+    const changePanel = (element) =>{
+      element.style.opacity="100"
+      let startValue=1;
+      let endValue=movie.rating;
+      console.log(endValue)
+      let duration = 30
+      let counter = setInterval(function(){
+        duration+=100
+        startValue+=0.1;
+        startValue = Math.round(startValue*10)/10
+        element.textContent=startValue;
+        if(startValue === endValue){
+          clearInterval(counter)
+        }
+      }, duration)
+      
+    }
+    ratingsShow.forEach(changePanel)
   }
 
     console.log(props)
@@ -28,15 +36,14 @@ function Panel(props){
 
     return(
       <div>
-        <div className='panel' id='panel' onClick={event => clickfunction[0](props.panelNumber,props.panelNumArr,clickfunction[1],props.setPanelNum)} style={{backgroundImage: `url(${movie.image})`, backgroundSize: "cover", backgroundPosition: "center center"}}>  
+        <div className='panel' id='panel' onClick={function(event){ clickfunction[0](props.panelNumber,props.panelNumArr,clickfunction[1],props.setPanelNum); test();}} style={{backgroundImage: `url(${movie.image})`, backgroundSize: "cover", backgroundPosition: "center center"}}>  
             <div id="panel-text">
               <div>{movie.name} ({movie.year})</div>
-              <div id="rating">1</div>
+              <div id="rating" className='rating'>{movie.rating}</div>
               <div>{movie.numVotes}</div>
-
             </div>  
         </div>
-        <button onClick={showRating}>hello</button>
+
       </div>
 
     )
